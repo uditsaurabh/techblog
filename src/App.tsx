de265349343e277627dd847fc8e742f59cf253f8
Udit Saurabh <udisaura@WKMZTBE8F38F.global.publicisgroupe.net>
2022-03-03 14:24:44 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello how are u
+    </div>
+  );
+}
+
+export default App;
